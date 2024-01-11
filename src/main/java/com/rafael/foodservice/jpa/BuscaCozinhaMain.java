bb307af8c27a1759dad7180021f62800d7aa8064
Rafael Moreira <rafaelsmoreiras@gmail.com>
2024-01-11 00:14:51 -0300

    feat: added method to find cozinda for id

---
+++ src/main/java/com/rafael/foodservice/jpa/BuscaCozinhaMain.java
@@ -0,0 +1,21 @@
+package com.rafael.foodservice.jpa;
+
+import com.rafael.foodservice.FoodServiceApplication;
+import com.rafael.foodservice.domain.model.Cozinha;
+import org.springframework.boot.WebApplicationType;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.context.ApplicationContext;
+
+public class BuscaCozinhaMain {
+    public static void main(String[] args) {
+        ApplicationContext applicationContext = new SpringApplicationBuilder(FoodServiceApplication.class)
+                .web(WebApplicationType.NONE) //Definindo que não é uma aplicação web
+                .run(args);
+
+        CadastroCozinha cadastroCozinha = applicationContext.getBean(CadastroCozinha.class);
+
+        Cozinha cozinha = cadastroCozinha.buscar(1L);
+
+        System.out.println(cozinha.getNome());
+    }
+}
